ffa20ee68d7adb0e5f93f311c9517983957b4371
Michael & Troy Lamerton <pair+MichaelDong32.troy-lamerton@devbootcamp.com>
2016-07-08 11:03:52 +1200

    Display hello world with react

--- client/index.js
+++ client/index.js
@@ -1,1 +1,14 @@
+/* Entry point for the app */
 console.log('hi');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app.js'
+
+import domready from 'domready'
+
+domready( () => {
+  ReactDOM.render(
+    <App />,
+    document.querySelector('#app')
+  )
+})
