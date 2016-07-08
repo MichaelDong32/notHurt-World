9b28cd42e871f60babdfcdcf0233f13c84e51d7b
shayan <shayan.akhoond82@gmail.com>
2016-07-08 12:27:54 +1200

    img and layout

--- client/components/home.js
+++ client/components/home.js
@@ -4,7 +4,19 @@ export default class Home extends React.Component{
   render() {
     return (
       <div>
-        <h1>Hello team fun</h1>
+        <div className="title-container">
+          <h1>Title goes here!</h1>
+          <h2>Description goes here</h2>
+        </div>
+        <div className="body-container">
+          <img src="./resources/images/body.svg" width="300px"/>
+          <div className="upper-body-div"></div>
+          <div className="mid-body-div"></div>
+          <div className="lower-body-div"></div>
+        </div>
+        <div className="disclaimer-container">
+          <h5>Disclaimer</h5>
+        </div>
         <button type="button">Click Me</button>
       </div>
       )
