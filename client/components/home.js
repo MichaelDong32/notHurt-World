9bbfa609ebf868e8d8c0f61b9e9bce40e532f19d
Michael & Troy Lamerton <pair+MichaelDong32.troy-lamerton@devbootcamp.com>
2016-07-08 15:23:16 +1200

    Move popup modal into home

--- client/components/home.js
+++ client/components/home.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Popup from './Popup.js'
+import Instructions from './Instructions'
+import Images from './Images'
 
 export default class Home extends React.Component{
+  constructor (props) {
+    super(props)
+    this.state = {
+      modalIsOpen: false
+      title: '',
+      instructions: [],
+      imageUrls: []
+    }
+  }
 
-  handleClick (event) {
-    console.log("you click")
+  openModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
   }
 
   render() {
@@ -14,18 +29,29 @@ export default class Home extends React.Component{
           <h1>Title goes here!</h1>
           <h2>Description goes here</h2>
         </div>
-
-        <div className="body-container" >
-          <img src="./resources/images/body.svg" className="stretch" alt="image" width="300px" />
-          <div className="upper-body-div" onClick={this.handleClick.bind(this)}></div>
-          <div className="mid-body-div" onClick={this.handleClick.bind(this)}></div>
-          <div className="lower-body-div"  onClick={this.handleClick.bind(this)}></div>
+        <div className="body-container">
+          <img src="./resources/images/body.svg" width="300px"/>
+          <div className="upper-body-div"></div>
+          <div className="mid-body-div"></div>
+          <div className="lower-body-div"></div>
         </div>
         <div className="disclaimer-container">
           <h5>Disclaimer</h5>
         </div>
         <button type="button" onClick={this.openModal} >Click Me</button>
-        <Popup  />
+
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          closeTimeoutMS={150}
+        >
+        <button className='close' onClick={this.closeModal}>[x]</button>
+          <h2>{this.state.title}</h2>
+          {this.state.instructions.map( (instruction, index) => {
+            return <p key={index}>{index + 1}. {instruction}</p>
+          })}
+          <Instructions instructions={this.state.instructions} />
+        </Modal>
       </div>
       )
   }
