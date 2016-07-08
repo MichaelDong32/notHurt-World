dafdf410fae835e85360a584d6bcd30c3ab9c13c
Michael & Troy Lamerton <pair+MichaelDong32.troy-lamerton@devbootcamp.com>
2016-07-08 14:04:14 +1200

    Modal popup works

--- client/components/Popup.js
+++ client/components/Popup.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import Instructions from './Instructions'
 import Images from './Images'
+import Modal from 'react-modal'
 
 export default class Popup extends React.Component {
-  render {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      modalOpen: true
+    }
+  }
+
+  toggleModal = () => {
+    this.setState({ modalOpen: !this.state.modalOpen });
+  }
+
+  render () {
     return (
       <div>
-        {this.props.title}
-        <Instructions value={this.props.instructions} />
-        <Images value={this.props.imageUrls}/>
+        <p>Title: {this.props.title}</p>
+        <Modal
+          isOpen={this.state.modalOpen}
+          onRequestClose={this.toggleModal}
+          closeTimeoutMS={150}
+        >
+          <h1>Modal Content</h1>
+          <h3>Stretch for: {this.props.title}</h3>
+          <p>Etc.</p>
+        </Modal>
       </div>
     )
   }
--- client/index.js
+++ client/index.js
@@ -3,12 +3,13 @@ console.log('hi');
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app.js'
+import Popup from './components/Popup.js'
 
 import domready from 'domready'
 
 domready( () => {
   ReactDOM.render(
-    <App />,
+    <Popup title="the popup title" />,
     document.querySelector('#app')
   )
 })
